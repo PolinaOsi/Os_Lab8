08dd4a080ea210851036c880b25b74835990aa22
Polina Osipova <76431671+PolinaOsi@users.noreply.github.com>
2021-11-17 16:42:54 +0700

    Update lab.c

--- lab.c
+++ lab.c
@@ -3,13 +3,13 @@
 #include <pthread.h>
 #include <string.h>
 #include <errno.h>
+#include <ctype.h>
 
 #define SUCCESS 0
 #define max_order_of_threads 5
 #define max_order_of_iterations 8
 #define count_of_args 3
 #define min_count_of_thread 1
-#define max_count_of_iterations 1000000000
 #define basis 10
 
 #define ERR_OF_COUNT_OF_ARGS 1
@@ -25,10 +25,16 @@ typedef struct param_of_thread {
     double part_of_pi;
 } param_of_thread;
 
-int isCorrect(int a, char* b) {
-    char buf[1024];
-    sprintf(buf, "%d", a);
-    return !strcmp(buf, b);
+int isCorrectFormOfArgs (char** argv, int j) {
+    int i = 0;
+    while(argv[j][i] != '\0') {
+        if(!isdigit(argv[j][i])) {
+            fprintf(stderr, "Use digits only\n");
+            exit(ERR_OF_FORM_OF_ARGS);
+        }
+        i++;
+    }
+    return SUCCESS;
 }
 
 void* startWork(void* param) {
@@ -55,17 +61,15 @@ int checkOfErrors(int argc, char** argv, int* count_of_threads, int* count_of_it
         return ERR_OF_COUNT_OF_ARGS;
     }
 
+    isCorrectFormOfArgs(argv, 1);
+    isCorrectFormOfArgs(argv, 2);
+
     if(strlen(argv[1]) > max_order_of_threads) {
         fprintf(stderr, "Too many threads\n");
         return ERR_OF_NUM_OF_THREADS;
     }
 
-    *count_of_threads = strtol(argv[1], (char**)NULL, basis);
-
-    if(!isCorrect(*count_of_threads, argv[1])) {
-        fprintf(stderr, "Use digits only\n");
-        return ERR_OF_FORM_OF_ARGS;
-    }
+    *count_of_threads = strtol(argv[1], NULL, basis);
 
     if(*count_of_threads < min_count_of_thread) {
         fprintf(stderr, "Too few threads\n");
@@ -77,18 +81,13 @@ int checkOfErrors(int argc, char** argv, int* count_of_threads, int* count_of_it
         return ERR_OF_NUM_OF_ITERATIONS;
     }
 
-    *count_of_iterations = strtol(argv[2], (char**)NULL, basis);
-    
-     if(!isCorrect(*count_of_iterations, argv[2])) {
-        fprintf(stderr, "Use digits only\n");
-        return ERR_OF_FORM_OF_ARGS;
-    }
+    *count_of_iterations = strtol(argv[1], NULL, basis);
 
     if(*count_of_iterations < *count_of_threads) {
         fprintf(stderr, "Too few iterations\n");
         return ERR_OF_NUM_OF_ITERATIONS;
     }
-    
+
     return SUCCESS;
 }
 
